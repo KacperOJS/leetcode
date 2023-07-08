281a6f0bfabad6ba2e19c5643b3e8ab5ed68c129
Kacper <kacperodziemczyk@gmail.com>
2023-07-08 22:05:12 +0200

    uuid make home.tsx looking good with css and delete useless files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,126 @@
-import Product from './Product';
-
+// import Product from './Product';
+import { v4 as uuidV4 } from 'uuid';
 interface Data {
-  title: string;
-  description: string;
-  info: string;
-  image: string;
+  id:string,
+  name:string,
+  href:string,
+  imageSrc: string,
+  imageAlt:string,
+  price: string,
+  color: string,
 }
 
 const Home = () => {
   const products: Data[] = [
-    {
-      title: 'Hoodie',
-      description: 'Made in China',
-      info: '95% Cotton 5% Polyester',
-      image: 'hoodie.jpg',
-    },
-    {
-      title: 'T-Shirt',
-      description: 'Made in USA',
-      info: '100% Cotton',
-      image: 'tshirt.jpg',
-    },
+	{
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  {
+		id: uuidV4(),
+		name: 'Basic Tee',
+		href: '#',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+		imageAlt: "Front of men's Basic Tee in black.",
+		price: '$35',
+		color: 'Black',
+	  },
+	  
     // Add more products as needed
   ];
 
   return (
-    <div>
-      {products.map((product, index) => (
-        <Product
-          key={index}
-          title={product.title}
-          Description={product.description}
-          info={product.info}
-          image={product.image}
-        />
-      ))}
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 };
 
 export default Home;
